90bf4aeeb7b50fdc8cd1324005235d23c73a6f99
Adrien DODERO <adrien.dodero@bts.sio>
2022-12-12 12:49:35 +0100

    Debut TP

---
+++ resources/installPackage.bat
@@ -0,0 +1,15 @@
+symfony composer require annotations 
+symfony composer require maker --dev
+symfony composer require symfony/apache-pack --dev 
+symfony composer require twig
+symfony composer require logger
+symfony composer require profiler --dev 
+symfony composer require debug --dev 
+symfony composer require symfony/form 
+symfony composer require symfony/validator 
+symfony composer require symfony/orm-pack 
+symfony composer require symfony/mime 
+symfony composer require symfony/asset 
+symfony composer require symfony/mailer
+symfony composer require sensio/framework-extra-bundle 
+symfony composer require symfony/security-bundle
